107650b3d6f687330e35d11e255a92e1c9a8572f
Ubuntu <ubuntu@ip-172-31-20-16.eu-west-1.compute.internal>
2019-08-04 09:28:59 +0000

    Added render-template, request, redirect, url_for libraries and home view

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = 'irish_dictionary'
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
 
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/home')
+def home():
+    return 'Home page'
 
 
 if __name__ == '__main__':
