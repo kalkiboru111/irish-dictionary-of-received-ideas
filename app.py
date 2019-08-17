da19d1edbed90fa08cd7621c6c3c44f665e53849
Ubuntu <ubuntu@ip-172-31-20-16.eu-west-1.compute.internal>
2019-08-17 12:45:53 +0000

    Installed flask_login and implemented login gatekeeping in app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length 
+from flask_login import login_required, current_user
 from bson.objectid import ObjectId
 import re
 import os
@@ -46,7 +47,6 @@ def register():
     return render_template('register.html')
 
 #Login Form - taken from Pretty Printed github
-@app.route('/')
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
@@ -68,18 +68,22 @@ def logout():
 
 
 # Home - displays dictionary.
+@app.route('/')
 @app.route('/get_terms')
+@login_required
 def get_terms():
     return render_template("terms.html", 
     terms=mongo.db.terms.find())
     
 # Add Term - provides users with form to add terms.
 @app.route('/add_term')
+@login_required
 def add_term():
     return render_template("add_term.html", 
     categories=mongo.db.categories.find())
 
 @app.route('/insert_term', methods=['POST'])
+@login_required
 def insert_term():
     terms = mongo.db.terms
     terms.insert_one(request.form.to_dict())
@@ -87,6 +91,7 @@ def insert_term():
 
 # Edit Term - provides users with form to edit terms.
 @app.route('/edit_term/<term_id>')
+@login_required
 def edit_term(term_id):
     the_term =  mongo.db.terms.find_one({"_id": ObjectId(term_id)})
     all_categories =  mongo.db.categories.find()
@@ -95,6 +100,7 @@ def edit_term(term_id):
                            
                            
 @app.route('/update_term/<term_id>', methods=['POST'])
+@login_required
 def update_term(term_id):
     terms = mongo.db.terms
     terms.update( {"_id": ObjectId(term_id)},
@@ -107,11 +113,14 @@ def update_term(term_id):
 
 #Vote functionality  
 @app.route('/vote/<term_id>')
+@login_required
 def vote(term_id):
-    the_term = mongo.db.terms.find_one({"_id": ObjectId(term_id)})
+    the_term = mongo.db.terms.find_one({"_id": ObjectId(term_id)},
+    {   'votes': request.form.get('votes')})
     return render_template('vote.html', the_term=the_term)
 
 @app.route('/update_votes/<term_id>', methods=['POST'])
+@login_required
 def update_votes(term_id):
     terms = mongo.db.terms
     totalVotes = mongo.db.terms.totalVotes()
@@ -126,24 +135,28 @@ def update_votes(term_id):
 
 # Delete Term - provides users with means to delete terms.
 @app.route('/delete_term/<term_id>')
+@login_required
 def delete_term(term_id):
     mongo.db.terms.remove({"_id": ObjectId(term_id)})
     return redirect(url_for('get_terms'))
 
 # Categories - displays all categories.
 @app.route('/get_categories')
+@login_required
 def get_categories():
     return render_template("categories.html", 
     categories=mongo.db.categories.find())
 
 # Edit Category - provides users with means to edit categories.
 @app.route('/edit_category/<category_id>')
+@login_required
 def edit_category(category_id):
     return render_template('editcategory.html',
     category=mongo.db.categories.find_one(
     {'_id': ObjectId(category_id)}))
 
 @app.route('/update_category/<category_id>', methods=['POST'])
+@login_required
 def update_category(category_id):
     mongo.db.categories.update(
         {'_id': ObjectId(category_id)},
@@ -152,12 +165,14 @@ def update_category(category_id):
 
 # Delete Category - provides users with means to delete category.
 @app.route('/delete__category/<category_id>')
+@login_required
 def delete_category(category_id):
     mongo.db.categories.remove(
         {'_id': ObjectId(category_id)})
     return redirect(url_for('get_categories'))
     
 @app.route('/insert_category', methods=['POST'])
+@login_required
 def insert_category():
     category_doc = {'category_name': request.form.get('category_name')},
     mongo.db.categories.insert_one(category_doc)
@@ -165,11 +180,13 @@ def insert_category():
 
 #Add Category - provides users with means to add categories. 
 @app.route('/add_category')
+@login_required
 def add_category():
     return render_template('addcategory.html')
 
 #Search - provides users with means to text search through terms, categories and definitions.  
 @app.route('/search', methods=['POST'])
+@login_required
 def search():
     query = request.form.get('query')
     results = mongo.db.terms.find({ "$text": { "$search": query } } )
