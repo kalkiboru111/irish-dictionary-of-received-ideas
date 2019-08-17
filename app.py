8e79bd04d3b4f7cd840df158f19c67ae63412c9d
Ubuntu <ubuntu@ip-172-31-20-16.eu-west-1.compute.internal>
2019-08-17 18:13:15 +0000

    Commented out unused modules

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ from flask_wtf import FlaskForm
 
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length 
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-from werkzeug.security import generate_password_hash, check_password_hash
+#from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+#from werkzeug.security import generate_password_hash, check_password_hash
 from bson.objectid import ObjectId
 import re
 import os
